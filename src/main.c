81a2f096aa91c0503ad6235231a02f3973c4180f
zengfeng <zeng_feng@163.com>
2019-12-17 10:39:56 +0800

    maiev

--- src/main.c
+++ src/main.c
@@ -1,5 +1,23 @@
 #include "../head/a.h"
 int main()
 {
-	tt();
+
+	int a[2][2] = {
+		{1,2},
+		{3,4},
+	};
+
+	int (*pa)[2] = a;
+	int *pb;
+
+	for(int i = 0; i< 2; i++){
+		for( int j =0;j<2; j++){
+			pb = &pa[i][j];
+			//printf(" %d ", pa[i][j]);
+			printf(" %d ", *pb);
+		}
+		printf("\n");
+	}
+
+	//tt();
 }
